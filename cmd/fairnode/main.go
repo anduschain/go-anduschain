0a941f5b040caf49083fa844351d44ce6e58381f
guitare <mongsig@gmail.com>
2018-10-23 08:55:59 +0900

    1~2과정 정리 수정

--- cmd/fairnode/main.go
+++ cmd/fairnode/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"github.com/anduschain/go-anduschain/fairnode/db"
-	"gopkg.in/urfave/cli.v1"
-	"log"
-	"os"
 	"github.com/anduschain/go-anduschain/fairnode"
+	"github.com/anduschain/go-anduschain/fairnode/db"
+	"log"
 )
 
 func main() {
 	// TODO : andus >> cli 프로그램에서 환경변수 및 운영변수를 세팅 할 수 있도록 구성...
 	/*
-	app := cli.NewApp()
-	app.Name = "fairnode"
-	app.Usage = "Fairnode for AndUsChain"
-	app.Flags = []cli.Flag{
-	}
-	app.Action = func(c *cli.Context) error {
-		return nil
-	}
-	app.Run(os.Args)
-	....
+		app := cli.NewApp()
+		app.Name = "fairnode"
+		app.Usage = "Fairnode for AndUsChain"
+		app.Flags = []cli.Flag{
+		}
+		app.Action = func(c *cli.Context) error {
+			return nil
+		}
+		app.Run(os.Args)
+		....
 	*/
 
 	// monggo DB 연결정보 획득..
@@ -38,10 +36,9 @@ func main() {
 
 	frnd.ListenUDP()
 
-
 	// TODO : TCP Listen PORT : 60001
-
 	frnd.ListenTCP()
 
-	go
+	frnd.manageActiveNode()
+
 }
