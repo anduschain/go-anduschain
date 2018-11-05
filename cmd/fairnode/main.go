b5d1a1f331a8b8635c4e6381b0fe34e49b994a00
guitare <mongsig@gmail.com>
2018-11-05 17:51:50 +0900

    Fairnode 빌드 및 기동시 초기 부분 수정

--- cmd/fairnode/main.go
+++ cmd/fairnode/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"github.com/anduschain/go-anduschain/fairnode/server"
 	"github.com/anduschain/go-anduschain/fairnode/server/db"
+	"gopkg.in/urfave/cli.v1"
 	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 	// TODO : andus >> cli 프로그램에서 환경변수 및 운영변수를 세팅 할 수 있도록 구성...
-	// TODO : andus >> 페어노드의 키스토어를 만들어서 서명 하고 검증할 수있게 해야됨.
 	/*
 		app := cli.NewApp()
 		app.Name = "fairnode"
@@ -21,6 +25,40 @@ func main() {
 		app.Run(os.Args)
 		....
 	*/
+	app := cli.NewApp()
+	app.Name = "puppeth"
+	app.Usage = "assemble and maintain private Ethereum networks"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "network",
+			Usage: "name of the network to administer (no spaces or hyphens, please)",
+		},
+		cli.IntFlag{
+			Name:  "loglevel",
+			Value: 3,
+			Usage: "log level to emit to the screen",
+		},
+		// TODO : andus >> 2018-11-05 init() 이 필요한 설정값들 추가할 것..
+	}
+	app.Action = func(c *cli.Context) error {
+		// Set up the logger to print everything and the random generator
+		//log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(c.Int("loglevel")), log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
+		log.Println(" @ Action START !! ")
+		rand.Seed(time.Now().UnixNano())
+
+		network := c.String("network")
+		if strings.Contains(network, " ") || strings.Contains(network, "-") {
+			log.Fatal("No spaces or hyphens allowed in network name")
+		}
+		// Start the wizard and relinquish control
+		//makeWizard(c.String("network")).run()
+
+		// TODO : andus >> 2018-11-05 init() 이 필요한 기능 추가할 것..
+
+		return nil
+	}
+	log.Println(" @ Run START !! ")
+	app.Run(os.Args)
 
 	// monggo DB 연결정보 획득..
 	_, err := db.New()
@@ -32,14 +70,12 @@ func main() {
 	// TODO : UDP Listen PORT : 60002
 	frnd, err := server.New()
 	if err != nil {
-		log.Fatal(err)
+		//log.(string(err), )
 	}
 
 	frnd.ListenUDP()
 
 	// TODO : TCP Listen PORT : 60001
 	frnd.ListenTCP()
-
-	//frnd.manageActiveNode()
 
 }
