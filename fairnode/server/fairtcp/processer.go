f2f433d2e4fd1ef0e78479d5471f6026e0b3bffb
apple <donghaa90@naver.com>
2019-03-12 17:17:13 +0900

    naming change

--- fairnode/server/fairtcp/processer.go
+++ fairnode/server/fairtcp/processer.go
@@ -18,11 +18,11 @@ const (
 	LeaguePercent     = 30 // 초기 sendLeague 가능한 percent
 	reducePercent     = 5  // 한번 다시 계산할때마다 줄어드는 percent
 
-	makeJoinTxSignal = 3 // 리그 리스트 전송 후 joinTx를 만들기까지의 대기시간
+	makeJoinTxSig = 3 // 리그 리스트 전송 후 joinTx를 만들기까지의 대기시간
 
 	finalBlockSig      = 10 // geth노드에게 블록 생성 메세지를 본낸뒤 geth노드들의 투표를 받고 확정된 블록을 보내주기까지의 시간
 	noVoteBolckLeagChn = 5  // 투표받은 블록이 없어 다음 리그를 시작할 시간(조회 횟수)
-	nextBlockMakeTerm  = 5  // 투표후 다음 블록 생성 신호 전달 term
+	nextBlockMakeTerm  = 5  // 블록생성 후  다음 블록 생성 신호 전달 term
 )
 
 func (fu *FairTcp) sendLeague(otprnHash common.Hash) {
@@ -43,7 +43,7 @@ func (fu *FairTcp) sendLeague(otprnHash common.Hash) {
 			if num >= fu.JoinTotalNum(fu.manager.GetUsingOtprn(), percent) && num > 0 {
 				fu.logger.Info("리그 전송", "리그 참여자 수", num)
 				fu.sendTcpAll(otprnHash, transport.SendLeageNodeList, enodes)
-				time.Sleep(makeJoinTxSignal * time.Second)
+				time.Sleep(makeJoinTxSig * time.Second)
 				fu.manager.GetMakeJoinTxCh() <- struct{}{}
 				return
 			}
