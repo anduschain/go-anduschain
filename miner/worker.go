cdb189fa1b829057f7e51c3041043e8238f369af
hakuna <kwangmin010@gmail.com>
2019-09-02 19:41:25 +0900

    fix : transaction size

--- miner/worker.go
+++ miner/worker.go
@@ -45,7 +45,7 @@ const (
 
 	// txChanSize is the size of channel listening to NewTxsEvent.
 	// The number is referenced from the size of tx pool.
-	txChanSize = 4096 * 10
+	txChanSize = 4096
 
 	// chainHeadChanSize is the size of channel listening to ChainHeadEvent.
 	chainHeadChanSize = 10
