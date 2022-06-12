84a0b2687dec93ddc7354521bd1715ff16476e4f
Metanonia <metanonia@outlook.kr>
2022-06-12 15:22:40 +0900

    gas_table_test.go

--- core/vm/gas_table_test.go
+++ core/vm/gas_table_test.go
@@ -16,7 +16,16 @@
 
 package vm
 
-import "testing"
+import (
+	"github.com/anduschain/go-anduschain/common"
+	"github.com/anduschain/go-anduschain/common/hexutil"
+	"github.com/anduschain/go-anduschain/common/math"
+	"github.com/anduschain/go-anduschain/core/state"
+	"github.com/anduschain/go-anduschain/ethdb"
+	"github.com/anduschain/go-anduschain/params"
+	"math/big"
+	"testing"
+)
 
 func TestMemoryGasCost(t *testing.T) {
 	//size := uint64(math.MaxUint64 - 64)
@@ -32,5 +41,63 @@ func TestMemoryGasCost(t *testing.T) {
 	_, err = memoryGasCost(&Memory{}, size+1)
 	if err == nil {
 		t.Error("expected error")
+	}
+}
+
+var eip2200Tests = []struct {
+	original byte
+	gaspool  uint64
+	input    string
+	used     uint64
+	refund   uint64
+	failure  error
+}{
+	{0, math.MaxUint64, "0x60006000556000600055", 10012, 0, nil},               // 0 -> 0 -> 0
+	{0, math.MaxUint64, "0x60006000556001600055", 25012, 0, nil},               // 0 -> 0 -> 1
+	{0, math.MaxUint64, "0x60016000556000600055", 25012, 15000, nil},           // 0 -> 1 -> 0
+	{0, math.MaxUint64, "0x60016000556002600055", 25012, 0, nil},               // 0 -> 1 -> 2
+	{0, math.MaxUint64, "0x60016000556001600055", 25012, 0, nil},               // 0 -> 1 -> 1
+	{1, math.MaxUint64, "0x60006000556000600055", 10012, 15000, nil},           // 1 -> 0 -> 0
+	{1, math.MaxUint64, "0x60006000556001600055", 25012, 15000, nil},           // 1 -> 0 -> 1
+	{1, math.MaxUint64, "0x60006000556002600055", 25012, 15000, nil},           // 1 -> 0 -> 2
+	{1, math.MaxUint64, "0x60026000556000600055", 10012, 15000, nil},           // 1 -> 2 -> 0
+	{1, math.MaxUint64, "0x60026000556003600055", 10012, 0, nil},               // 1 -> 2 -> 3
+	{1, math.MaxUint64, "0x60026000556001600055", 10012, 0, nil},               // 1 -> 2 -> 1
+	{1, math.MaxUint64, "0x60026000556002600055", 10012, 0, nil},               // 1 -> 2 -> 2
+	{1, math.MaxUint64, "0x60016000556000600055", 10012, 15000, nil},           // 1 -> 1 -> 0
+	{1, math.MaxUint64, "0x60016000556002600055", 10012, 0, nil},               // 1 -> 1 -> 2
+	{1, math.MaxUint64, "0x60016000556001600055", 10012, 0, nil},               // 1 -> 1 -> 1
+	{0, math.MaxUint64, "0x600160005560006000556001600055", 45018, 15000, nil}, // 0 -> 1 -> 0 -> 1
+	{1, math.MaxUint64, "0x600060005560016000556000600055", 30018, 30000, nil}, // 1 -> 0 -> 1 -> 0
+	{1, 2306, "0x6001600055", 2306, 0, ErrOutOfGas},                            // 1 -> 1 (2300 sentry + 2xPUSH)
+	{1, 2307, "0x6001600055", 2307, 0, nil},                                    // 1 -> 1 (2301 sentry + 2xPUSH)
+}
+
+func TestEIP2200(t *testing.T) {
+	for i, tt := range eip2200Tests {
+		address := common.BytesToAddress([]byte("contract"))
+
+		statedb, _ := state.New(common.Hash{}, state.NewDatabase(ethdb.NewMemDatabase()))
+		statedb.CreateAccount(address)
+		statedb.SetCode(address, hexutil.MustDecode(tt.input))
+		statedb.SetState(address, common.Hash{}, common.BytesToHash([]byte{tt.original}))
+		statedb.Finalise(true) // Push the state into the "original" slot
+
+		vmctx := Context{
+			CanTransfer: func(StateDB, common.Address, *big.Int) bool { return true },
+			Transfer:    func(StateDB, common.Address, common.Address, *big.Int) {},
+		}
+		vmenv := NewEVM(vmctx, statedb, params.AllDebProtocolChanges, Config{})
+
+		_, gas, err := vmenv.Call(AccountRef(common.Address{}), address, nil, tt.gaspool, new(big.Int))
+		if err != tt.failure {
+			t.Errorf("test %d: failure mismatch: have %v, want %v", i, err, tt.failure)
+		}
+		if used := tt.gaspool - gas; used != tt.used {
+			t.Errorf("test %d: gas used mismatch: have %v, want %v", i, used, tt.used)
+		}
+		if refund := vmenv.StateDB.GetRefund(); refund != tt.refund {
+			t.Errorf("test %d: gas refund mismatch: have %v, want %v", i, refund, tt.refund)
+		}
 	}
 }
