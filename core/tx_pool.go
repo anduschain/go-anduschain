33d44ecfbd365d7b8583aed3eaca0f81eb2be692
Metanonia <metanonia@outlook.kr>
2022-06-22 20:25:34 +0900

    accounts>abi>bind>util_test.go

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -637,9 +637,9 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 	case types.GeneralTx, types.EthTx:
 		// Drop non-local transactions under our own minimal accepted gas price
 		local = local || pool.locals.contains(from) // account may be local even if the transaction arrived from the network
-		//if !local && pool.gasPrice.Cmp(tx.GasPrice()) > 0 {
-		//	return ErrUnderpriced
-		//}
+		if !local && pool.gasPrice.Cmp(tx.GasPrice()) > 0 {
+			return ErrUnderpriced
+		}
 		if pool.gasPrice.Cmp(tx.GasPrice()) > 0 {
 			return ErrUnderpriced
 		}
