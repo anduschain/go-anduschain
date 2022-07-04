c3e81ca4d53d86550b54c27d11f0c68e183dd86b
Metanonia <metanonia@outlook.kr>
2022-07-04 12:02:23 +0900

    graphql>graphql_test.go

--- graphql/schema.go
+++ graphql/schema.go
@@ -69,12 +69,6 @@ const schema string = `
         transaction: Transaction!
     }
 
-    #EIP-2718
-    type AccessTuple{
-        address: Address!
-        storageKeys : [Bytes32!]!
-    }
-
     # Transaction is an Ethereum transaction.
     type Transaction {
         # Hash is the hash of this transaction.
