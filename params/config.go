e2ecf91779c20f363cf3fd0fb8fceb47ce04c2e5
Metanonia <metanonia@outlook.kr>
2024-02-28 13:42:38 +0900

    push0 disable..

--- params/config.go
+++ params/config.go
@@ -592,8 +592,7 @@ func (c *ChainConfig) Rules(num *big.Int) Rules {
 	if chainID == nil {
 		chainID = new(big.Int)
 	}
-	fmt.Printf("====== CSW chainID %v Byzantium %v Constaninople %v Pohang %v\n",
-		chainID, c.ByzantiumBlock, c.ConstantinopleBlock, c.PohangBlock)
+
 	if chainID.Int64() == 3355 { // TESTNET
 		return Rules{
 			ChainID:          new(big.Int).Set(chainID),
